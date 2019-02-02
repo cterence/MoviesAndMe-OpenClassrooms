1d6615c5276ef5b872c18acd255f5d2433f0f1fd
Térence Chateigné <terence.chateigne@etu.utc.fr>
2019-02-02 11:51:57 +0100

    Amélioration recherche

--- Components/Search.js
+++ Components/Search.js
@@ -1,18 +1,30 @@
 import React from 'react'
-
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
 
 
 class Search extends React.Component {
 
   render() {
     return (
-      <View>
-        <TextInput placeholder='Titre du film'/>
-        <Button title='Rechercher' onPress={() => {}}/>
+      <View style ={{ marginTop: 30}}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+        <Button style={{ height: 50 }} title='Rechercher' onPress={() => {}}/>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  textinput : {
+     height: 50,
+     marginLeft: 5,
+     marginRight: 5,
+     height: 50,
+     borderColor: '#000000',
+     borderWidth: 1,
+     paddingLeft: 5
+  }
+
+})
 
 export default Search
